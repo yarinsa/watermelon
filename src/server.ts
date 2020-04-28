402c7126402b333dcbc6794d9b41f28122097104
yarinsa <yarinsasson2@gmail.com>
2020-04-28 12:36:45 +0300

    fwf

--- src/server.ts
+++ src/server.ts
@@ -17,9 +17,9 @@ app.use(
     cookie: { secure: false },
   })
 );
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
 
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, "build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
@@ -27,7 +27,6 @@ app.get("/*", (req, res) => {
 app.get("/ping", function (req, res) {
   return res.send("pong");
 });
-
 apolloServer.applyMiddleware({ app });
 
 const port = process.env.PORT || 3030;
