2137fe4e728e10ff6cce996d275eeefb7087428c
yarinsa <yarinsasson2@gmail.com>
2020-04-28 13:20:11 +0300

    added logs for debugging on heroku

--- src/server.ts
+++ src/server.ts
@@ -22,14 +22,17 @@ app.use(
 app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, "build")));
 app.get("/*", (req, res) => {
+  console.log("request has been made!");
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.get("/ping", function (req, res) {
+  console.log(" just got pingged!");
   return res.send("pong");
 });
 
 app.post("/ping2", function (req, res) {
+  console.log(" just got pingged 2!");
   return res.send("pong2");
 });
 
