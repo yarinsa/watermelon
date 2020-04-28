c07f2c367593792b71bf6ac62a455fe99b547c60
yarinsa <yarinsasson2@gmail.com>
2020-04-28 03:31:49 +0300

    server configiration

--- src/server.ts
+++ src/server.ts
@@ -19,10 +19,10 @@ app.use(
 );
 app.use(cors());
 // app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 apolloServer.applyMiddleware({ app });
 
